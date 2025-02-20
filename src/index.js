9e6d13d3835f65036abebefb033669b3e503168b
IvelisBecker <ivelisbecker@gmail.com>
2025-02-20 14:00:01 -0600

    Added querySelectors and ids on index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-const practice = document.querySelector(#practiceLog); 
+const prCard = document.querySelector('#practiceCard')
+const prLog = document.querySelector('#practiceLog'); 
 const savePrBtn = document.querySelector('#save');
 const closePrBtn = document.querySelector('#close');
 
-// const sb = document.querySelector('#save')
-// const cb = document.querySelector('#close')
-// const del = document.querySelector('#delete-msg')
-// const deleteIt = document.querySelector('#del-yes')
-// const cb2 = document.querySelector('#no')
 
-// Function to call filled Practice Logs from local storage
-function getPractice() {
-    const prLog = JSON.parse(localStorage.getItem("practiceLog"))
-    return prLog
-}
-
-//     let logs = getPractice()
-//     if (logs === null) {
-//         console.log(`Please add your practice log.`)
-//         return 
-//     } else {
-//         for (i=0; i < logs.lenght; i++) {
-
-//         }
-//     }
-//     return
-}
