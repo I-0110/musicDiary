cdaef7fefebb51fb89a48d1b44754a23620cc763
I-0110 <ivelisbecker@gmail.com>
2025-04-14 20:26:48 -0500

    Delete client directory

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom';
-
-document.body.innerHTML = '<div id="app"></div>';
-
-const root = createRoot(document.getElementById('app'));
-
-root.render(<h1>Music Time!</h1>);
-
